595ea040c3a6156be9d5a783adcd37ef445151ce
刘玺 <liux1@itcast.cn>
2019-06-14 18:33:03 +0800

    外观和样式调整

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
+
 import App from './App'
 
 // 导入antd-mobile的样式：
 import 'antd-mobile/dist/antd-mobile.css'
+
+// 注意：我们自己写的全局样式需要放在组件库样式后面导入，这样，样式才会生效！因为后面的样式会覆盖前面同名的样式
+import './index.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))
