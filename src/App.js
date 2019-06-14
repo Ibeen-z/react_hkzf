186edd41e6319eb0b5a751d1dbb0bc4fc2fdba11
刘玺 <liux1@itcast.cn>
2019-06-14 15:31:44 +0800

    antd-mobile的使用

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react'
 
+// 导入要使用的组件
+import { Button } from 'antd-mobile'
+
 function App() {
-  return <div className="App">项目根组件</div>
+  return (
+    <div className="App">
+      项目根组件 <Button>登录</Button>
+    </div>
+  )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -3,4 +3,7 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 
+// 导入antd-mobile的样式：
+import 'antd-mobile/dist/antd-mobile.css'
+
 ReactDOM.render(<App />, document.getElementById('root'))
