a06c932b3227d655071d76d55caf632bfe435f73
刘玺 <liux1@itcast.cn>
2019-06-20 11:52:29 +0800

    首页导航菜单

--- src/pages/Index/index.js
+++ src/pages/Index/index.js
@@ -1,19 +1,47 @@
 import React from 'react'
 
 // 导入组件
-import { Carousel } from 'antd-mobile'
+import { Carousel, Flex } from 'antd-mobile'
 
 // 导入axios
 import axios from 'axios'
 
-/* 
-  1 安装 axios：yarn add axios 。
-  2 在 Index 组件中导入 axios 。
-  3 在 state 中添加轮播图数据：swipers 。
-  4 新建一个方法 getSwipers 用来获取轮播图数据，并更新 swipers 状态。
-  5 在 componentDidMount 钩子函数中调用该方法。
-  6 使用获取到的数据渲染轮播图。
-*/
+// 导入导航菜单图片
+import Nav1 from '../../assets/images/nav-1.png'
+import Nav2 from '../../assets/images/nav-2.png'
+import Nav3 from '../../assets/images/nav-3.png'
+import Nav4 from '../../assets/images/nav-4.png'
+
+// 导入样式文件
+import './index.css'
+
+// 导航菜单数据
+const navs = [
+  {
+    id: 1,
+    img: Nav1,
+    title: '整租',
+    path: '/home/list'
+  },
+  {
+    id: 2,
+    img: Nav2,
+    title: '合租',
+    path: '/home/list'
+  },
+  {
+    id: 3,
+    img: Nav3,
+    title: '地图找房',
+    path: '/map'
+  },
+  {
+    id: 4,
+    img: Nav4,
+    title: '去出租',
+    path: '/rent'
+  }
+]
 
 export default class Index extends React.Component {
   state = {
@@ -54,12 +82,29 @@ export default class Index extends React.Component {
     ))
   }
 
+  // 渲染导航菜单
+  renderNavs() {
+    return navs.map(item => (
+      <Flex.Item
+        key={item.id}
+        onClick={() => this.props.history.push(item.path)}
+      >
+        <img src={item.img} alt="" />
+        <h2>{item.title}</h2>
+      </Flex.Item>
+    ))
+  }
+
   render() {
     return (
       <div className="index">
+        {/* 轮播图 */}
         <Carousel autoplay infinite autoplayInterval={5000}>
           {this.renderSwipers()}
         </Carousel>
+
+        {/* 导航菜单 */}
+        <Flex className="nav">{this.renderNavs()}</Flex>
       </div>
     )
   }
