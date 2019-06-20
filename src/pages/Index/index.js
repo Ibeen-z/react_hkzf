6b363af73ad8c82800320d2346c181a65d6eafb7
刘玺 <liux1@itcast.cn>
2019-06-20 10:01:30 +0800

    首页轮播图（1）

--- src/pages/Index/index.js
+++ src/pages/Index/index.js
@@ -1,7 +1,50 @@
 import React from 'react'
 
+// 导入组件
+import { Carousel } from 'antd-mobile'
+
 export default class Index extends React.Component {
+  state = {
+    // 图片的名称
+    data: ['1', '2', '3']
+  }
+
+  componentDidMount() {
+    // simulate img loading
+    setTimeout(() => {
+      this.setState({
+        data: [
+          'AiyWuByWklrrUDlFignR',
+          'TekJlZRVCjLFexlOCuWn',
+          'IJOtIlfsYdTyaDTRVrLI'
+        ]
+      })
+    }, 100)
+  }
+
   render() {
-    return <div>这是首页</div>
+    return (
+      <div className="index">
+        <Carousel autoplay infinite autoplayInterval={5000}>
+          {this.state.data.map(val => (
+            <a
+              key={val}
+              href="http://itcast.cn"
+              style={{
+                display: 'inline-block',
+                width: '100%',
+                height: 212
+              }}
+            >
+              <img
+                src={`https://zos.alipayobjects.com/rmsportal/${val}.png`}
+                alt=""
+                style={{ width: '100%', verticalAlign: 'top' }}
+              />
+            </a>
+          ))}
+        </Carousel>
+      </div>
+    )
   }
 }
