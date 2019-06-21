c0fd1d8dd5cd245ee5a13affdb5255fd0301b6d3
刘玺 <liux1@itcast.cn>
2019-06-21 10:44:19 +0800

    顶部导航

--- src/pages/Index/index.js
+++ src/pages/Index/index.js
@@ -163,6 +163,35 @@ export default class Index extends React.Component {
           ) : (
             ''
           )}
+
+          {/* 搜索框 */}
+          <Flex className="search-box">
+            {/* 左侧白色区域 */}
+            <Flex className="search">
+              {/* 位置 */}
+              <div
+                className="location"
+                onClick={() => this.props.history.push('/citylist')}
+              >
+                <span className="name">上海</span>
+                <i className="iconfont icon-arrow" />
+              </div>
+
+              {/* 搜索表单 */}
+              <div
+                className="form"
+                onClick={() => this.props.history.push('/search')}
+              >
+                <i className="iconfont icon-seach" />
+                <span className="text">请输入小区或地址</span>
+              </div>
+            </Flex>
+            {/* 右侧地图图标 */}
+            <i
+              className="iconfont icon-map"
+              onClick={() => this.props.history.push('/map')}
+            />
+          </Flex>
         </div>
 
         {/* 导航菜单 */}
