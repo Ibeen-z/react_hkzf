d051270bc2735aa8e707cd5a90834e3a464daf90
刘玺 <liux1@itcast.cn>
2019-06-22 17:14:19 +0800

    reactt-virtualized基本使用

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import App from './App'
 // 导入antd-mobile的样式：
 import 'antd-mobile/dist/antd-mobile.css'
 
+// 导入 react-virtualized 组件的样式
+import 'react-virtualized/styles.css'
+
 // 导入字体图标库的样式文件
 import './assets/fonts/iconfont.css'
 
--- src/pages/CityList/index.js
+++ src/pages/CityList/index.js
@@ -1,26 +1,14 @@
 import React from 'react'
 
-// 导入axios
 import axios from 'axios'
-
-// 1 导入 NavBar组件
 import { NavBar } from 'antd-mobile'
 
-// 导入样式
-import './index.scss'
+// 导入 List 组件
+import { List } from 'react-virtualized'
 
+import './index.scss'
 // 导入 utils 中获取当前定位城市的方法
 import { getCurrentCity } from '../../utils'
-
-/*
-// 接口返回的数据格式：
-[{ "label": "北京", "value": "", "pinyin": "beijing", "short": "bj" }]
-
-// 渲染城市列表的数据格式为：
-{ a: [{}, {}], b: [{}, ...] }
-// 渲染右侧索引的数据格式：
-['a', 'b']
-*/
 
 // 数据格式化的方法
 // list: [{}, {}]
@@ -52,6 +40,33 @@ const formatCityData = list => {
   }
 }
 
+/* 
+  1 安装：yarn add react-virtualized 。
+  2 在项目入口文件 index.js 中导入样式文件（只导入一次即可）。
+  3 打开文档，点击 List 组件，进入 List 的文档中。
+  4 翻到文档最底部，将示例代码拷贝到项目中。
+  5 分析示例代码。
+*/
+
+// 列表数据的数据源
+const list = Array(100).fill('react-virtualized')
+
+// 渲染每一行数据的渲染函数
+// 函数的返回值就表示最终渲染在页面中的内容
+function rowRenderer({
+  key, // Unique key within array of rows
+  index, // 索引号
+  isScrolling, // 当前项是否正在滚动中
+  isVisible, // 当前项在 List 中是可见的
+  style // 注意：重点属性，一定要给每一个行数据添加该样式！作用：指定每一行的位置
+}) {
+  return (
+    <div key={key} style={style}>
+      1232 -{list[index]} {index} {isScrolling + ''}
+    </div>
+  )
+}
+
 export default class CityList extends React.Component {
   componentDidMount() {
     this.getCityList()
@@ -60,27 +75,15 @@ export default class CityList extends React.Component {
   // 获取城市列表数据的方法
   async getCityList() {
     const res = await axios.get('http://localhost:8080/area/city?level=1')
-    // console.log('城市列表数据：', res)
     const { cityList, cityIndex } = formatCityData(res.data.body)
 
-    /* 
-      1 获取热门城市数据
-      2 将数据添加到 cityList 中
-      3 将索引添加到 cityIndex 中
-    */
+    // 获取热门城市数据
     const hotRes = await axios.get('http://localhost:8080/area/hot')
-    // console.log('热门城市数据：', hotRes)
     cityList['hot'] = hotRes.data.body
-    // 将索引添加到 cityIndex 中
     cityIndex.unshift('hot')
 
     // 获取当前定位城市
     const curCity = await getCurrentCity()
-
-    /* 
-      1 将当前定位城市数据添加到 cityList 中
-      2 将当前定位城市的索引添加到 cityIndex 中
-    */
     cityList['#'] = [curCity]
     cityIndex.unshift('#')
 
@@ -99,6 +102,15 @@ export default class CityList extends React.Component {
         >
           城市选择
         </NavBar>
+
+        {/* 城市列表 */}
+        <List
+          width={300}
+          height={300}
+          rowCount={list.length}
+          rowHeight={50}
+          rowRenderer={rowRenderer}
+        />
       </div>
     )
   }
