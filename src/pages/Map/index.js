46fbc54ca279bd3bf03c3366f5388ece10a46893
刘玺 <liux1@itcast.cn>
2019-06-25 12:11:45 +0800

    绘制房源覆盖物

--- src/pages/Map/index.js
+++ src/pages/Map/index.js
@@ -10,6 +10,17 @@ import styles from './index.module.css'
 // 解决脚手架中全局变量访问的问题
 const BMap = window.BMap
 
+// 覆盖物样式
+const labelStyle = {
+  cursor: 'pointer',
+  border: '0px solid rgb(255, 0, 0)',
+  padding: '0px',
+  whiteSpace: 'nowrap',
+  fontSize: '12px',
+  color: 'rgb(255, 255, 255)',
+  textAlign: 'center'
+}
+
 export default class Map extends React.Component {
   componentDidMount() {
     this.initMap()
@@ -37,20 +48,37 @@ export default class Map extends React.Component {
           map.addControl(new BMap.ScaleControl())
 
           /* 
-            1 创建 Label 实例对象。
-            2 调用 setStyle() 方法设置样式。
-            3 在 map 对象上调用 addOverlay() 方法，将文本覆盖物添加到地图中。
+            1 调用 Label 的 setContent() 方法，传入 HTML 结构，修改 HTML 内容的样式。
+            2 调用 setStyle() 修改覆盖物样式。
+            3 给文本覆盖物添加单击事件。
+
+            <div class="${styles.bubble}">
+              <p class="${styles.name}">${name}</p>
+              <p>${num}套</p>
+            </div>
           */
           const opts = {
-            position: point
-            // offset: new BMap.Size(30, -30)
+            position: point,
+            offset: new BMap.Size(-35, -35)
           }
 
-          const label = new BMap.Label('文本覆盖物', opts)
+          // 说明：设置 setContent 后，第一个参数中设置的文本内容就失效了，因此，直接清空即可
+          const label = new BMap.Label('', opts)
+
+          // 设置房源覆盖物内容
+          label.setContent(`
+            <div class="${styles.bubble}">
+              <p class="${styles.name}">浦东</p>
+              <p>99套</p>
+            </div>
+          `)
 
           // 设置样式
-          label.setStyle({
-            color: 'green'
+          label.setStyle(labelStyle)
+
+          // 添加单击事件
+          label.addEventListener('click', () => {
+            console.log('房源覆盖物被点击了')
           })
 
           // 添加覆盖物到地图中
