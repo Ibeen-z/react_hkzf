87fa0938dbde35156a42b73797744bfba5eb52c3
刘玺 <liux1@itcast.cn>
2019-06-26 19:55:49 +0800

    实现找房页面顶部导航栏（结构和样式）

--- src/components/SearchHeader/index.js
+++ src/components/SearchHeader/index.js
@@ -2,23 +2,21 @@ import React from 'react'
 
 import { Flex } from 'antd-mobile'
 
-// 导入路由的高阶组件
 import { withRouter } from 'react-router-dom'
+
+import PropTypes from 'prop-types'
 
 // 导入样式
 import './index.scss'
 
-function SearchHeader({ history }) {
-  // 从本地存储中获取当前定位城市名称
-  const curCityName = JSON.parse(localStorage.getItem('hkzf_city')).label
-
+function SearchHeader({ history, cityName, className }) {
   return (
-    <Flex className="search-box">
+    <Flex className={['search-box', className || ''].join(' ')}>
       {/* 左侧白色区域 */}
       <Flex className="search">
         {/* 位置 */}
         <div className="location" onClick={() => history.push('/citylist')}>
-          <span className="name">{curCityName}</span>
+          <span className="name">{cityName}</span>
           <i className="iconfont icon-arrow" />
         </div>
 
@@ -32,6 +30,12 @@ function SearchHeader({ history }) {
       <i className="iconfont icon-map" onClick={() => history.push('/map')} />
     </Flex>
   )
+}
+
+// 添加属性校验
+SearchHeader.propTypes = {
+  cityName: PropTypes.string.isRequired,
+  className: PropTypes.string
 }
 
 export default withRouter(SearchHeader)
--- src/pages/HouseList/index.js
+++ src/pages/HouseList/index.js
@@ -1,15 +1,28 @@
 import React from 'react'
 
-// 导入顶部搜索导航组件
+import { Flex } from 'antd-mobile'
+
+// 导入搜索导航栏组件
 import SearchHeader from '../../components/SearchHeader'
 
+// 导入样式
 import styles from './index.module.css'
 
+// 获取当前定位城市信息
+const { label } = JSON.parse(localStorage.getItem('hkzf_city'))
+
+/*
+  1 在找房页面 SearchHeader 组件基础上，调整结构（添加返回icon等）。
+  2 给 SearchHeader 组件传递 className 属性，来调整组件样式，让其适应找房页面效果。
+*/
 export default class HouseList extends React.Component {
   render() {
     return (
-      <div className={styles.root}>
-        <SearchHeader />
+      <div>
+        <Flex className={styles.header}>
+          <i className="iconfont icon-back" />
+          <SearchHeader cityName={label} className={styles.searchHeader} />
+        </Flex>
       </div>
     )
   }
