41e9ddd4128f4b536cadc745b99451286745560e
刘玺 <liux1@itcast.cn>
2019-06-27 14:58:29 +0800

    FilterTitle组件实现

--- src/pages/HouseList/components/Filter/index.js
+++ src/pages/HouseList/components/Filter/index.js
@@ -6,8 +6,38 @@ import FilterMore from '../FilterMore'
 
 import styles from './index.module.css'
 
+// 标题高亮状态
+// true 表示高亮； false 表示不高亮
+const titleSelectedStatus = {
+  area: false,
+  mode: false,
+  price: false,
+  more: false
+}
+
 export default class Filter extends Component {
+  state = {
+    titleSelectedStatus
+  }
+
+  // 点击标题菜单实现高亮
+  // 注意：this指向的问题！！！
+  // 说明：要实现完整的功能，需要后续的组件配合完成！
+  onTitleClick = type => {
+    this.setState(prevState => {
+      return {
+        titleSelectedStatus: {
+          // 获取当前对象中所有属性的值
+          ...prevState.titleSelectedStatus,
+          [type]: true
+        }
+      }
+    })
+  }
+
   render() {
+    const { titleSelectedStatus } = this.state
+
     return (
       <div className={styles.root}>
         {/* 前三个菜单的遮罩层 */}
@@ -15,7 +45,10 @@ export default class Filter extends Component {
 
         <div className={styles.content}>
           {/* 标题栏 */}
-          <FilterTitle />
+          <FilterTitle
+            titleSelectedStatus={titleSelectedStatus}
+            onClick={this.onTitleClick}
+          />
 
           {/* 前三个菜单对应的内容： */}
           {/* <FilterPicker /> */}
--- src/pages/HouseList/components/FilterTitle/index.js
+++ src/pages/HouseList/components/FilterTitle/index.js
@@ -12,16 +12,37 @@ const titleList = [
   { title: '筛选', type: 'more' }
 ]
 
-export default function FilterTitle() {
+/* 
+  1 通过 props 接收，高亮状态对象 titleSelectedStatus。
+  2 遍历 titleList 数组，渲染标题列表。
+  3 判断高亮对象中当前标题是否高亮，如果是，就添加高亮类。
+
+  4 给标题项绑定单击事件，在事件中调用父组件传过来的方法 onClick。
+  5 将当前标题 type，通过 onClick 的参数，传递给父组件。
+  6 父组件中接收到当前 type，修改该标题的选中状态为true。
+*/
+
+export default function FilterTitle({ titleSelectedStatus, onClick }) {
   return (
     <Flex align="center" className={styles.root}>
-      <Flex.Item>
-        {/* 选中类名： selected */}
-        <span className={[styles.dropdown, styles.selected].join(' ')}>
-          <span>区域</span>
-          <i className="iconfont icon-arrow" />
-        </span>
-      </Flex.Item>
+      {titleList.map(item => {
+        // item.type => 'area'
+        const isSelected = titleSelectedStatus[item.type]
+        return (
+          <Flex.Item key={item.type} onClick={() => onClick(item.type)}>
+            {/* 选中类名： selected */}
+            <span
+              className={[
+                styles.dropdown,
+                isSelected ? styles.selected : ''
+              ].join(' ')}
+            >
+              <span>{item.title}</span>
+              <i className="iconfont icon-arrow" />
+            </span>
+          </Flex.Item>
+        )
+      })}
     </Flex>
   )
 }
