fd0d484281ba10bd211402ca85a85f09293ab843
刘玺 <liux1@itcast.cn>
2019-06-29 13:12:47 +0800

    使用List组件渲染数据

--- src/pages/HouseList/index.js
+++ src/pages/HouseList/index.js
@@ -2,11 +2,15 @@ import React from 'react'
 
 import { Flex } from 'antd-mobile'
 
+import { List } from 'react-virtualized'
+
 import { API } from '../../utils/api'
+import { BASE_URL } from '../../utils/url'
 
 // 导入搜索导航栏组件
 import SearchHeader from '../../components/SearchHeader'
 import Filter from './components/Filter'
+import HouseItem from '../../components/HouseItem'
 // 导入样式
 import styles from './index.module.css'
 
@@ -56,6 +60,31 @@ export default class HouseList extends React.Component {
     this.searchHouseList()
   }
 
+  /* 
+    1 封装 HouseItem 组件，实现 Map 和 HouseList 页面中，房屋列表项的复用。
+    2 使用 HouseItem 组件改造 Map 组件的房屋列表项。
+    3 使用 react-virtualized 的 List 组件渲染房屋列表（参考 CityList 组件的使用）。
+  */
+  renderHouseList = ({ key, index, style }) => {
+    // 根据索引号来获取当前这一行的房屋数据
+    const { list } = this.state
+    const house = list[index]
+
+    console.log(house)
+
+    return (
+      <HouseItem
+        key={key}
+        style={style}
+        src={BASE_URL + house.houseImg}
+        title={house.title}
+        desc={house.desc}
+        tags={house.tags}
+        price={house.price}
+      />
+    )
+  }
+
   render() {
     return (
       <div className={styles.root}>
@@ -72,7 +101,15 @@ export default class HouseList extends React.Component {
         <Filter onFilter={this.onFilter} />
 
         {/* 房屋列表 */}
-        <div className={styles.houseItems}>{/* 房屋列表内容 */}</div>
+        <div className={styles.houseItems}>
+          <List
+            width={300}
+            height={300}
+            rowCount={this.state.count} // List列表项的行数
+            rowHeight={120} // 每一行的高度
+            rowRenderer={this.renderHouseList} // 渲染列表项中的每一行
+          />
+        </div>
       </div>
     )
   }
