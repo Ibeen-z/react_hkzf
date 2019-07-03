a313a73c9960cee78a12a8530d7ca38db8b40840
刘玺 <liux1@itcast.cn>
2019-07-03 18:13:26 +0800

    使用formik重构登录功能

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -3,6 +3,9 @@ import { Flex, WingBlank, WhiteSpace, Toast } from 'antd-mobile'
 
 import { Link } from 'react-router-dom'
 
+// 导入withFormik
+import { withFormik } from 'formik'
+
 // 导入 API
 import { API } from '../../utils'
 
@@ -15,22 +18,22 @@ import styles from './index.module.css'
 // const REG_PWD = /^[a-zA-Z_\d]{5,12}$/
 
 /* 
-  登录功能：
+  使用 formik 重构登录功能：
 
-  1 添加状态：username（账号） 和 password（密码）。
-  2 使用受控组件方式获取表单元素值。
-  3 给 form 表单添加 onSubmit。
-  4 创建方法 handleSubmit，实现表单提交。
-
-  5 在方法中，通过 username 和 password 获取到账号和密码。
-  6 使用 API 调用登录接口，将 username 和 password 作为参数。
-  7 判断返回值 status 为 200 时，表示登录成功。
-  8 登录成功后，将 token 保存到本地存储中（hkzf_token）。
-  9 返回登录前的页面。
+  1 安装：yarn add formik。
+  2 导入 withFormik，使用 withFormik 高阶组件包裹 Login 组件。
+  3 为 withFormik 提供配置对象：mapPropsToValues / handleSubmit。
+  4 在 Login 组件中，通过 props 获取到 values（表单元素值对象）、 handleSubmit、handleChange。
+  5 使用 values 提供的值，设置为表单元素的 value，使用 handleChange 设置为表单元素的 onChange。
+    注意：在给表单元素设置 handleChange 的时候，为了让其生效，需要给 表单元素 添加 name 属性，并且 name 属性的值与当前value名称相同！
+    
+  6 使用 handleSubmit 设置为表单的 onSubmit 。
+  7 在 handleSubmit 中，通过 values 获取到表单元素值。
+  8 在 handleSubmit 中，完成登录逻辑。
 */
 
 class Login extends Component {
-  state = {
+  /* state = {
     username: '',
     password: ''
   }
@@ -73,10 +76,13 @@ class Login extends Component {
       // 登录失败
       Toast.info(description, 2, null, false)
     }
-  }
+  } */
 
   render() {
-    const { username, password } = this.state
+    // const { username, password } = this.state
+
+    // 通过 props 获取高阶组件传递进来的属性
+    const { values, handleSubmit, handleChange } = this.props
 
     return (
       <div className={styles.root}>
@@ -86,12 +92,12 @@ class Login extends Component {
 
         {/* 登录表单 */}
         <WingBlank>
-          <form onSubmit={this.handleSubmit}>
+          <form onSubmit={handleSubmit}>
             <div className={styles.formItem}>
               <input
                 className={styles.input}
-                value={username}
-                onChange={this.getUserName}
+                value={values.username}
+                onChange={handleChange}
                 name="username"
                 placeholder="请输入账号"
               />
@@ -101,8 +107,8 @@ class Login extends Component {
             <div className={styles.formItem}>
               <input
                 className={styles.input}
-                value={password}
-                onChange={this.getPassword}
+                value={values.password}
+                onChange={handleChange}
                 name="password"
                 type="password"
                 placeholder="请输入密码"
@@ -127,4 +133,38 @@ class Login extends Component {
   }
 }
 
+// 使用 withFormik 高阶组件包装 Login 组件，为 Login 组件提供属性和方法
+Login = withFormik({
+  // 提供状态：
+  mapPropsToValues: () => ({ username: '', password: '' }),
+  // 表单的提交事件
+  handleSubmit: async (values, { props }) => {
+    // 获取账号和密码
+    const { username, password } = values
+
+    // console.log('表单提交了', username, password)
+    // 发送请求
+    const res = await API.post('/user/login', {
+      username,
+      password
+    })
+
+    console.log('登录结果：', res)
+    const { status, body, description } = res.data
+
+    if (status === 200) {
+      // 登录成功
+      localStorage.setItem('hkzf_token', body.token)
+
+      // 注意：无法在该方法中，通过 this 来获取到路由信息
+      // 所以，需要通过 第二个对象参数中获取到 props 来使用 props
+      props.history.go(-1)
+    } else {
+      // 登录失败
+      Toast.info(description, 2, null, false)
+    }
+  }
+})(Login)
+
+// 注意：此处返回的是 高阶组件 包装后的组件
 export default Login
