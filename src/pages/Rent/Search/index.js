1c3b76a6912158b1f52b51ed1eec8d12927d8e03
刘玺 <liux1@itcast.cn>
2019-07-07 20:53:22 +0800

    关键词搜索小区信息

--- src/pages/Rent/Search/index.js
+++ src/pages/Rent/Search/index.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react'
 
 import { SearchBar } from 'antd-mobile'
 
-import { getCity } from '../../../utils'
+import { getCity, API } from '../../../utils'
 
 import styles from './index.module.css'
 
 export default class Search extends Component {
   // 当前城市id
   cityId = getCity().value
+  // 定时器id
+  timerId = null
 
   state = {
     // 搜索框的值
@@ -27,6 +29,45 @@ export default class Search extends Component {
     ))
   }
 
+  /* 
+    关键词搜索小区信息
+
+    1 给 SearchBar 组件，添加 onChange 配置项，获取文本框的值。
+    2 判断当前文本框的值是否为空。
+    3 如果为空，清空列表，然后 return，不再发送请求。
+    4 如果不为空，使用 API 发送请求，获取小区数据。
+    5 使用定时器 setTimeout 来延迟搜索，提升性能。
+  */
+  handleSearchTxt = value => {
+    this.setState({ searchTxt: value })
+
+    if (!value) {
+      // 文本框的值为空
+      return this.setState({
+        tipsList: []
+      })
+    }
+
+    // 清除上一次的定时器
+    clearTimeout(this.timerId)
+
+    this.timerId = setTimeout(async () => {
+      // 获取小区数据
+      const res = await API.get('/area/community', {
+        params: {
+          name: value,
+          id: this.cityId
+        }
+      })
+
+      // console.log(res)
+
+      this.setState({
+        tipsList: res.data.body
+      })
+    }, 500)
+  }
+
   render() {
     const { history } = this.props
     const { searchTxt } = this.state
@@ -37,6 +78,7 @@ export default class Search extends Component {
         <SearchBar
           placeholder="请输入小区或地址"
           value={searchTxt}
+          onChange={this.handleSearchTxt}
           showCancelButton={true}
           onCancel={() => history.go(-1)}
         />
